7785ba104c3f62d56beedcd16d30f6a55a660521
David Case <Dav3xor@gmail.com>
2016-10-15 23:56:28 -0700

    added more instructions to assembler

--- masm.cpp
+++ masm.cpp
@@ -14,10 +14,25 @@ uint64_t MetisASM::get_uint64(void) {
 
 MetisASM::MetisASM() : 
   handlers({
-    {"ERROR",               [this](MetisVM &m, char *s) -> void {  m.add_error(); } },
-    {"END",                 [this](MetisVM &m, char *s) -> void {  m.add_end(); } }, 
-    {"NOOP",                [this](MetisVM &m, char *s) -> void {  m.add_noop(); } }, 
-    {"JUMP",                [this](MetisVM &m, char *s) -> void {  m.add_jump(this->get_addr_mode()); } } 
+    {"ERROR",               [this](MetisVM &m, char *s) -> void {  m.add_error      (); } },
+    {"END",                 [this](MetisVM &m, char *s) -> void {  m.add_end        (); } }, 
+    {"NOOP",                [this](MetisVM &m, char *s) -> void {  m.add_noop       (); } }, 
+    {"JUMP",                [this](MetisVM &m, char *s) -> void {  m.add_jump       (this->get_addr_mode()); } },
+    {"JUMPI",               [this](MetisVM &m, char *s) -> void {  m.add_jumpi      (this->get_uint64());   } },
+    {"JIZ",                 [this](MetisVM &m, char *s) -> void {  m.add_jizz       (this->get_addr_mode(),
+                                                                                     this->get_addr_mode()); } }, 
+    {"JNZ",                 [this](MetisVM &m, char *s) -> void {  m.add_jnz        (this->get_addr_mode(),
+                                                                                     this->get_addr_mode()); } },
+    {"JNE",                 [this](MetisVM &m, char *s) -> void {  m.add_jne        (this->get_addr_mode(),
+                                                                                     this->get_addr_mode(),
+                                                                                     this->get_uint64()); } }, 
+    {"JMPE",                [this](MetisVM &m, char *s) -> void {  m.add_jmpe       (this->get_addr_mode(),
+                                                                                     this->get_addr_mode(),
+                                                                                     this->get_uint64()); } },
+    {"STO",                 [this](MetisVM &m, char *s) -> void {  m.add_store      (this->get_addr_mode(),
+                                                                                     this->get_addr_mode()); } },
+    {"STI",                 [this](MetisVM &m, char *s) -> void {  m.add_storei     (this->get_addr_mode(),
+                                                                                     this->get_uint64()); } } 
   }),
   addr_modes({
     {"REGA",        REGA},
