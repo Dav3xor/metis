233df8af2e885dee55444d4f607e52422caa6c29
David Case <Dav3xor@gmail.com>
2016-12-08 00:07:57 -0800

    better tentative support for .h and .c files

--- metiscompiler.c
+++ metiscompiler.c
@@ -1,15 +1,15 @@
-#include "mpc.h"
+#include "../mpc/mpc.h"
+#include "grammar.h"
 
 #define PARSER(name, symbol) mpc_parser_t *name      = mpc_new("symbol");
+
 int main(int argc, char **argv) {
-  int fd;
-  fd
   mpc_result_t r;
 
   PARSER(Unsigned,unsigned);
-  PARSER(Integer, integer);
   PARSER(Float,   float);
   PARSER(Label,   label);
   PARSER(Vector,  vector);
   PARSER(Matrix,  matrix);
 
+  int fd = open(
