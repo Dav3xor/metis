ca7b12cb28c6cccf49103b8008e8304f1d8d372a
David Case <Dav3xor@gmail.com>
2016-12-09 21:57:28 -0800

    more work on parser

--- metiscompiler.c
+++ metiscompiler.c
@@ -6,13 +6,19 @@
 int main(int argc, char **argv) {
   mpc_result_t r;
 
-  PARSER(Unsigned,unsigned);
+  PARSER(Unsigned,xunsigned);
   PARSER(Float,   float);
   PARSER(Label,   label);
   PARSER(Vector,  vector);
   PARSER(Matrix,  matrix);
 
-  mpca_lang(MPCA_LANG_PREDICTIVE, grammar_txt,
+  mpca_lang(MPCA_LANG_DEFAULT, grammar_txt,
             Unsigned, Float, Label, Vector, Matrix, NULL);
 
+  mpc_print(Unsigned);
+  mpc_print(Float);
+  mpc_print(Label);
+  mpc_print(Vector);
+  mpc_print(Matrix);
 }
+
