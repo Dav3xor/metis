bb4edb99d0054acc412ed6ddc58a81d28f9b6db9
David Case <Dav3xor@gmail.com>
2016-12-15 20:25:49 -0800

    attempting to make parser work (doesn't yet)

--- metiscompiler.c
+++ metiscompiler.c
@@ -6,19 +6,37 @@
 int main(int argc, char **argv) {
   mpc_result_t r;
 
+  PARSER(String,  string);
   PARSER(Unsigned,xunsigned);
+  PARSER(Integer, integer);
   PARSER(Float,   float);
   PARSER(Label,   label);
   PARSER(Vector,  vector);
   PARSER(Matrix,  matrix);
+  PARSER(Factor,  factor);
+  PARSER(Term,  term);
+  PARSER(Lexp,  lexp);
+  PARSER(Typeident,  typeident);
+  PARSER(Args,  args);
+  PARSER(Function,  function);
+  PARSER(Stmt,  stmt);
+  PARSER(Metis,  metis);
 
   mpca_lang(MPCA_LANG_DEFAULT, grammar_txt,
-            Unsigned, Float, Label, Vector, Matrix, NULL);
+            String, Unsigned, Integer, Float, Label, Vector, Matrix, 
+            Factor, Term, Lexp, Typeident, Args, Function, Stmt, Metis, NULL);
 
   mpc_print(Unsigned);
   mpc_print(Float);
   mpc_print(Label);
   mpc_print(Vector);
   mpc_print(Matrix);
+  if(mpc_parse_contents("test.m", Metis, &r)) {
+    mpc_ast_print(r.output);
+    mpc_ast_delete(r.output);
+  } else {
+    mpc_err_print(r.error);
+    mpc_err_delete(r.error);
+  }
 }
 
