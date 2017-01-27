17e42b61e0e0d490f9cc762b70218b09d8077dbe
David Case <Dav3xor@gmail.com>
2017-01-26 23:02:16 -0800

    work on start_handler

--- metiscompiler.c
+++ metiscompiler.c
@@ -11,7 +11,7 @@ typedef struct parser_state_t {
   char *last_float;
 }parser_state;
 
-typedef mpc_ast_t *(*grammar_handler)(parser_state *, mpc_ast_trav_t *traveller);
+typedef void (*grammar_handler)(parser_state *, mpc_ast_trav_t *traveller);
 
 typedef struct handler_t {
   char            handle[128];
@@ -20,36 +20,39 @@ typedef struct handler_t {
 } handler;
 
 
-mpc_ast_trav_t * handle_start(parser_state *state, mpc_ast_trav_t *contents) {
+void handle_start(parser_state *state, mpc_ast_trav_t *contents) {
+  mpc_ast_t *ast_next = mpc_ast_traverse_next(&contents);
   ast_next = mpc_ast_traverse_next(&contents);
+  ast_next = mpc_ast_traverse_next(&contents);
+  printf("%s\n", ast_next->contents);
   printf("Tag: %s -- %d -- %s\n", ast_next->tag, ast_next->state, ast_next->contents);
 }
   
-mpc_ast_trav_t * handle_comment(parser_state *state, mpc_ast_trav_t *contents) {
+void handle_comment(parser_state *state, mpc_ast_trav_t *contents) {
   // pass
   printf("%s\n", contents);
 }
 
-mpc_ast_trav_t *handle_label(parser_state *state, mpc_ast_trav_t *contents) {
+void handle_label(parser_state *state, mpc_ast_trav_t *contents) {
   state->last_label = contents;
   printf("%s\n", contents);
 }
 
-mpc_ast_trav_t *handle_string(parser_state *state, mpc_ast_trav_t *contents) {
+void handle_string(parser_state *state, mpc_ast_trav_t *contents) {
   state->last_string = contents;
   printf("%s\n", contents);
 }
 
-mpc_ast_trav_t *handle_integer(parser_state *state, mpc_ast_trav_t *contents) {
+void handle_integer(parser_state *state, mpc_ast_trav_t *contents) {
   state->last_integer = contents;
   printf("%s\n", contents);
 }
 
-mpc_ast_trav_t *handle_float(parser_state *state, mpc_ast_trav_t *contents) {
+void handle_float(parser_state *state, mpc_ast_trav_t *contents) {
   state->last_float = contents;
   printf("%s\n", contents);
 }
-mpc_ast_trav_t *handle_fcall(parser_state *state, mpc_ast_trav_t *contents) {
+void handle_fcall(parser_state *state, mpc_ast_trav_t *contents) {
   printf("fcall start\n");
 }
 
