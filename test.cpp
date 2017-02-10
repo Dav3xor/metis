47d76ee507fe9973bf08c1b963c1d997d24fc6d3
David Case <Dav3xor@gmail.com>
2017-02-09 19:04:09 -0800

    added tests for load_sr instruction

--- test.cpp
+++ test.cpp
@@ -470,7 +470,7 @@ TEST_CASE( "storei", "[MetisVM]" ) {
   REQUIRE( m.cur_stack_val() == 5);
 }
 
-TEST_CASE( "store_sr/load_sr", "[MetisVM]" ) {
+TEST_CASE( "store_sr", "[MetisVM]" ) {
   uint8_t buf[10000];
   uint64_t stack[5];
   MetisVM m(buf,10000, stack, 5, NULL, 0);
@@ -491,10 +491,32 @@ TEST_CASE( "store_sr/load_sr", "[MetisVM]" ) {
   m.add_end();
 
   m.eval();
-  printf("%d %d %d\n",m.get_registers()[REGA],m.get_registers()[REGB],m.get_registers()[REGC]);
+  
   REQUIRE( m.get_registers()[REGA] == 3);
   REQUIRE( m.get_registers()[REGB] == 20);
   REQUIRE( m.get_registers()[REGC] == 10);
+}
+TEST_CASE( "load_sr", "[MetisVM]" ) {
+  uint8_t buf[10000];
+  uint64_t stack[5];
+  MetisVM m(buf,10000, stack, 5, NULL, 0);
+  m.hard_reset();
+  
+  m.add_storei(STACK_PUSH, 1);
+  m.add_storei(STACK_PUSH, 2);
+  m.add_storei(STACK_PUSH, 3);
+  
+  m.add_load_sr(0, REGA);
+  m.add_load_sr(1, REGB);
+  m.add_load_sr(2, REGC);
+
+  m.add_end();
+
+  m.eval();
+  printf("%d %d %d\n",m.get_registers()[REGA],m.get_registers()[REGB],m.get_registers()[REGC]);
+  REQUIRE( m.get_registers()[REGA] == 3);
+  REQUIRE( m.get_registers()[REGB] == 2);
+  REQUIRE( m.get_registers()[REGC] == 1);
 }
 
 TEST_CASE( "noop", "[MetisVM]" ) {
