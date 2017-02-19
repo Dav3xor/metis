9546db99d8a01f2c8843e0e74c8d05055c239b56
David Case <Dav3xor@gmail.com>
2017-02-18 23:08:35 -0800

    added handler for statements

--- metiscompiler.c
+++ metiscompiler.c
@@ -23,6 +23,7 @@ typedef struct handler_t {
 handler *handlers = NULL;
 handler *bshandlers = NULL;
 handler *blockhandlers = NULL;
+handler *stmthandlers = NULL;
 
 void handle_start(parser_state *state, mpc_ast_trav_t *contents) {
   mpc_ast_t *ast_next = mpc_ast_traverse_next(&contents);
@@ -74,6 +75,9 @@ void handle_block(parser_state *state, mpc_ast_trav_t *contents) {
 }
 
 void handle_stmt(parser_state *state, mpc_ast_trav_t *contents) {
+  // pass
+}
+void handle_return(parser_state *state, mpc_ast_trav_t *contents) {
   // pass
 }
 
@@ -132,7 +136,7 @@ void handle_function(parser_state *state, mpc_ast_trav_t *contents) {
     ast_next = mpc_ast_traverse_next(&contents);
     num_arguments += 1;
   }
-  printf("??? %s %s\n",ast_next->tag,ast_next->contents);
+  //printf("??? %s %s\n",ast_next->tag,ast_next->contents);
   if(!(strcmp(ast_next->contents, "<-"))) {
     ast_next = mpc_ast_traverse_next(&contents);
     return_type = ast_next->contents;
@@ -140,7 +144,6 @@ void handle_function(parser_state *state, mpc_ast_trav_t *contents) {
   }
   // next token must be the :, so consume it.
   ast_next = mpc_ast_traverse_next(&contents);
-  printf("x: %s\n", ast_next->contents);
   while(strcmp(ast_next->contents, "fin")) {
     handle_bs(state, contents);
     ast_next = mpc_ast_traverse_next(&contents);
@@ -186,6 +189,7 @@ handler bs_handlers[] = { {"bs|comment|longcomment|regex",   &handle_comment},
                           {"bs|block|>",                     &handle_block},
                           {"bs|stmt|>",                      &handle_stmt}
                         };
+
 handler block_handlers[] = { {"type",                          &handle_type},
                              {"if",                            &handle_if},
                              {"include",                       &handle_include},
@@ -193,9 +197,13 @@ handler block_handlers[] = { {"type",                          &handle_type},
                              {"for",                           &handle_for},
                              {"def",                           &handle_def} 
                            };
+
+handler stmt_handlers[] =  { {"return|>",                      &handle_return}
+                           };
 unsigned int num_handlers = sizeof(handler_defs)/sizeof(handler);
 unsigned int num_bs_handlers = sizeof(bs_handlers)/sizeof(handler);
 unsigned int num_block_handlers = sizeof(block_handlers)/sizeof(handler);
+unsigned int num_stmt_handlers = sizeof(stmt_handlers)/sizeof(handler);
 
 
 int main(int argc, char **argv) {
@@ -249,6 +257,10 @@ int main(int argc, char **argv) {
   for(int i=0; i<num_block_handlers; i++) {
     handler *cur = &(block_handlers[i]);
     HASH_ADD_STR(blockhandlers, handle, cur);
+  }
+  for(int i=0; i<num_stmt_handlers; i++) {
+    handler *cur = &(stmt_handlers[i]);
+    HASH_ADD_STR(stmthandlers, handle, cur);
   }
 
   mpca_lang(MPCA_LANG_DEFAULT, (char *)grammar_txt,
