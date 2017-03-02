afa78ea744dda18223b37626ce17baf325e461a1
David Case <Dav3xor@gmail.com>
2017-03-01 22:55:27 -0800

    fix for pop_label_context

--- compiler_util.c
+++ compiler_util.c
@@ -12,14 +12,14 @@ void push_label_context(parser_state *state)
 
 void pop_label_context(parser_state *state)
 {
-  //label *cur, temp;
+  label *cur, *temp;
   if(state->cur_context == 0) {
     printf("attempt to pop context 0\n");
     exit(1);
   }
-//  HASH_ITER(hh, state->label_contexts[state->cur_context], cur, temp) {
-//    HASH_DEL(state->label_contexts[state->cur_context], cur);
-//    free(cur);
-//  }
+  HASH_ITER(hh, state->label_contexts[state->cur_context], cur, temp) {
+    HASH_DEL(state->label_contexts[state->cur_context], cur);
+    free(cur);
+  }
   state->cur_context -= 1;
 } 
