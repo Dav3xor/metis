d3ac547b114d0f78bf55b0fb542d05cc184659b2
David Case <Dav3xor@gmail.com>
2017-03-11 16:38:18 -0800

    more work on string compare macro

--- compiler.h
+++ compiler.h
@@ -8,7 +8,7 @@
 #define PARSER(name, symbol) mpc_parser_t *name      = mpc_new(symbol);
 
 #define FILL_HASH {NULL, NULL, NULL, NULL, NULL, NULL, 0, 0}
-#define CONTAINS (!(strcmp(ast_next->tag, "args|>")))
+#define CMP(a,b) (!(strcmp(a, b)))
 typedef struct label_t {
   char *label;
   uint64_t value;
