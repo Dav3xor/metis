cb68776032cbf7336e16b1cc1f9c4c6fb32f8ac5
David Case <Dav3xor@gmail.com>
2017-03-17 21:09:03 -0700

    added missing call to add_label

--- grammar_handlers.c
+++ grammar_handlers.c
@@ -207,6 +207,7 @@ void handle_function(parser_state *state, mpc_ast_trav_t *contents) {
     ast_next = mpc_ast_traverse_next(&contents);
     char *var  = ast_next->contents;
     printf("ARG: %s %s\n",type, var);
+    add_label(state, var, num_arguments);
     ast_next = mpc_ast_traverse_next(&contents);
     num_arguments += 1;
   }
