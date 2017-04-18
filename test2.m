38dd2bd8de9e360dc08f03397fdb0435229c3a97
David Case <Dav3xor@gmail.com>
2017-04-17 23:20:23 -0700

    moved most of test.m to test2.m (to make testing easier)

---
+++ test2.m
@@ -0,0 +1,87 @@
+
+def complex float x, float y <- float:
+  <- x*(x+y)*5+7.
+fin
+
+def return_x float x <- float:
+  <- x.
+fin
+
+; shortcomment
+;another short comment
+
+def is_positive float x <- bool:
+  if x > 0
+    <- true.
+  else
+    <- false.
+  fin
+fin
+
+def is_zero float x <- bool:
+  if x > 0
+    <- false.
+  else if x < 0
+    <- false.
+  else
+    <- true.
+  fin
+fin
+
+def divide float x, float y <- float:
+  if {is_zero y} = true
+    <-! divide_by_zero_error.
+  fin
+
+  float result := x/y.
+  <- result.
+fin
+
+
+float x := {square 5, 2}.
+
+if x > 1
+  print "x > 1.5!".
+fin
+
+matrix y := | 1.0 -2 -3.0 |
+            | 4    5  6 |
+            | 7    8  9 |.
+
+float z := (x+y)*6 + y.
+float z1 := 5.
+float z2 := 5.0.
+float z3 := -5.
+float z1 := -5.0.
+float z1 := 5.
+
+
+string s := "quick brown fox.".
+unsigned u := 5.
+integer i := -5.
+
+for integer x := 0 ; x < 5 ; 1
+  print "x".
+fin
+
+:magnitude <- float.
+:print.
+
+type point
+  float x.
+  float y.
+
+  :magnitude
+    <- {sqrt x*x + y*y}.
+  fin
+
+  :print
+    print "x={x} y={y}".
+  fin
+fin
+
+include
+  - stuff
+  - things
+fin
+
