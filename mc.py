0720328bc711107edbdade281051d76f720b0b8a
David Case <Dav3xor@gmail.com>
2017-04-27 22:32:19 -0700

    started on making dictionaries for handling dispatching

--- mc.py
+++ mc.py
@@ -1,5 +1,15 @@
 # an attempt at a compiler using shlex...
 import shlex
+
+block_handlers = {'if':        handle_if,
+                  'include':   handle_include,
+                  'while':     handle_while,
+                  'for':       handle_for,
+                  'def':       handle_functiondef,
+                  'type':      handle_typedef}
+
+
+
 with open("test.m","r") as input:
   input = input.read()
 
