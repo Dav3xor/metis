00cf2d7022de8a75ce464c61ffeaad0e236aa79d
David Case <Dav3xor@gmail.com>
2017-06-17 22:18:00 -0700

    added matrix to test.m

--- test.m
+++ test.m
@@ -10,6 +10,10 @@ fin
 
 def square float x <- float:
   print "hello".
+  matrix m := | 1  2  3  4  |
+              | 5  6  7  8  |
+              | 9  10 11 12 |
+              | 13 14 15 16 |.
   <- x * x.
 fin
 
